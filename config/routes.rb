425b536f648c6751f53e61060e38cb515c0de9f8
asatokoki <okometabero@me.com>
2021-11-06 10:54:56 +0000

    [Finish?]多分いけてるっぽい

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about', to: 'homes#about', as: :about
   devise_for :users
-  resources :users
+  resources :users do
+     resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
